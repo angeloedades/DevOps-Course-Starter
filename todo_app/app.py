258aca0d68f857d93380168b76777b5781d65931
Angelo Edades <6472602+angeloedades@users.noreply.github.com>
2021-01-21 17:26:15 +0000

    Module 1 Added render_template() import Added sessions import Modified index route to serve template with items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import todo_app.data.session_items as session
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello World!'
+    items = session.get_items()
+    return render_template("index.html", items=items)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
